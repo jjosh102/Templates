301e6adc56e27a37160007d9cb95252885fc982b
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2023-05-23 15:01:33 +0800

    Added webapp-wasm template

---
+++ src/.template.base/webapp-wasm/src/Application/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Obaki.Template.Application;
+public class Class1
+{
+
+}
---
+++ src/.template.base/webapp-wasm/tests/Obaki.Template.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Obaki.Template.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
